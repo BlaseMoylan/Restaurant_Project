601004be91ee1f390200bb10f3001c7df540228d
Blase Moylan <moylanblase@gmail.com>
2023-03-02 11:21:09 -0800

    Fix: made validation method more flexible

--- validation.py
+++ validation.py
@@ -16,9 +16,13 @@ class Order_Validation:
     #             print('this is not one of the options! Please order something else.')
     @staticmethod
     def real_order(list_of_options):
+        index=0
+        for item in list_of_options:
+            print(f'{item.dish_name}: {item.price} press {index}')
+            index+=1
         user_input=False
         while user_input==False:
-            user_order=input(f'what are you ordering? \npress 0 for Pizza\npress 1 for Pasta\npress 2 for Salad')
+            user_order=input('what are you ordering? ')
             if user_order.isnumeric():
                 user_order=int(user_order)
                 if user_order<len(list_of_options):
