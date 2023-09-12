844b7d096ef7a185f5a21396c0a2b993a6de8276
Blase Moylan <moylanblase@gmail.com>
2023-09-12 15:26:48 -0700

    📖 DOC: added documentation to franchise

--- franchise.py
+++ franchise.py
@@ -1,15 +1,39 @@
-# ask a user what food they would like to order
-#call the static OrderFactory.create_order() method 
-# to instantiate an order object.
-#call the logger.log_transaction() method 
-# to log the order to the log.txt file
+
 from logger import logger
 from order_factory import Order_Factory
 from validation import Order_Validation
 
 class Franchise:
-    def __init__(self,location_number:int) -> None:
-        self.location_number=location_number
+    """
+    The `Franchise` class represents a franchise store in a retail system.
+
+    Attributes:
+    - location_number (int): The unique identifier for the franchise store.
+
+    Methods:
+    - __init__(self, location_number: int): Initializes a new franchise store with a specified location number.
+    - place_order(self): Places an order at the franchise store using the Order_Factory and logs the transaction.
+
+    Example Usage:
+    >>> store_one = Franchise(1)
+    >>> store_one.place_order()
+    """
+
+    def __init__(self, location_number: int) -> None:
+        """
+        Initializes a new franchise store with a specified location number.
+
+        Args:
+        - location_number (int): The unique identifier for the franchise store.
+        """
+        self.location_number = location_number
+
     def place_order(self):
-        order=Order_Factory.create_order()
-        logger.log_transaction(order,self.location_number)
+        """
+        Places an order at the franchise store using the Order_Factory and logs the transaction.
+        """
+        # Create an order using the Order_Factory
+        order = Order_Factory.create_order()
+
+        # Log the transaction, associating it with the store's location number
+        logger.log_transaction(order, self.location_number)
