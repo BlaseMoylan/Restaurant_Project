cd4f5532bf7c366be82652855391fba8f0e3accb
Blase Moylan <moylanblase@gmail.com>
2023-09-12 15:24:30 -0700

    📖 DOC: added documentation to simulation

--- simulation.py
+++ simulation.py
@@ -1,15 +1,30 @@
 from franchise import Franchise
 from  Log_transaction_validation import Log_Transaction_validation
 class Simulation:
-    def run_simulation(self):
-        #a run_simulation() method to act as a facade pattern. 
-        #Instantiate 3 separate Franchise objects.
-        #Call place_order() on each franchise object multiple times.
-        Log_Transaction_validation.log_transaction_validation()
-        store_one= Franchise(1)
-        store_two= Franchise(2)
-        store_three= Franchise(3)
+    """
+    The `Simulation` class represents a simulation of a franchise-based store system.
 
+    Methods:
+    - run_simulation(): Runs the simulation by creating franchise instances, placing orders, and logging transactions.
+
+    Example Usage:
+    >>> simulation = Simulation()
+    >>> simulation.run_simulation()
+    """
+
+    def run_simulation(self):
+        """
+        Runs the simulation by creating franchise instances, placing orders, and logging transactions.
+        """
+        # Perform transaction validation logging
+        Log_Transaction_validation.log_transaction_validation()
+
+        # Create franchise instances
+        store_one = Franchise(1)
+        store_two = Franchise(2)
+        store_three = Franchise(3)
+
+        # Place orders at different franchise stores
         store_one.place_order()
         store_two.place_order()
         store_one.place_order()
